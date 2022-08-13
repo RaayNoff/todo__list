bb6089c259253b9b64ae3f495c979b200f6025ec
RaayNoff <kraxtv471365@gmail.com>
2022-08-13 13:21:08 +0700

    Changed styles in createList, installed react-select

--- src/components/UI/colorPicker/index.tsx
+++ src/components/UI/colorPicker/index.tsx
@@ -6,40 +6,11 @@ interface IColorPickerProps {
   onColorClicked: (color: string) => void;
 }
 
-const palette: { hex: string; name: string }[] = [
-  { hex: "#8B8B8B", name: "Аспидно-серый" },
-  { hex: "#daa520", name: "Золотисто-березовый" },
-  { hex: "#78DBE2", name: "Аквамариновый Крайола" },
-  { hex: "#FF2400", name: "Алый" },
-];
-
 const ColorPicker: FC<IColorPickerProps> = ({ onColorClicked }) => {
-  const [prevSelectedColor, setPrevSelectedColor] = useState<HTMLElement>();
-
   return (
     <section className={s.colorPicker}>
-      <header className={s.colorPicker__title}>Цвет</header>
-      <div className={s.colorPicker__colors}>
-        {palette.map((c) => (
-          <section
-            key={c.hex}
-            className={s.exactColor}
-            onClick={(e) => {
-              prevSelectedColor?.classList.remove(s.selectedColor);
-              e.currentTarget.classList.toggle(s.selectedColor);
-              setPrevSelectedColor(e.currentTarget);
-
-              onColorClicked(c.hex);
-            }}
-          >
-            <div
-              className={s.exactColor__dot}
-              style={{ background: c.hex }}
-            ></div>
-            <span className={s.exactColor__name}>{c.name}</span>
-          </section>
-        ))}
-      </div>
+      <header className={s.colorPicker__title}>Выберите цвет</header>
+      <div className={s.colorPicker__colors}></div>
     </section>
   );
 };
