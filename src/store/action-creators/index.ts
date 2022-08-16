cffa1176935576e4b9172be2caab0da5b332dec7
RaayNoff <kraxtv471365@gmail.com>
2022-08-16 20:12:35 +0700

    Added action creator for list fetching

--- src/store/action-creators/index.ts
+++ src/store/action-creators/index.ts
@@ -1,7 +1,9 @@
 import * as AuthorizationActionCreators from "./authorization";
 import * as RegistrationActionCreators from "./registration";
+import * as ListsActionCreators from "./lists";
 
 export default {
   ...AuthorizationActionCreators,
   ...RegistrationActionCreators,
+  ...ListsActionCreators,
 };
---
+++ src/store/action-creators/lists.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { AnyAction, Dispatch } from "redux";
+import { ListsAction, ListsActionTypes } from "../../types/list";
+import { BackendApiUrls } from "../../types/urls";
+
+export const fetchLists = (userId: string) => {
+  return async (dispatch: Dispatch<ListsAction | AnyAction>) => {
+    try {
+      dispatch({ type: ListsActionTypes.FETCH_LISTS });
+
+      const response = await axios.get(BackendApiUrls.FETCH_LISTS);
+
+      dispatch({
+        type: ListsActionTypes.FETCH_LISTS_SUCCES,
+        payload: response.data,
+      });
+    } catch (error) {
+      dispatch({
+        type: ListsActionTypes.FETCH_LISTS_ERROR,
+        payload: "Запрос на получение списков дел не был обработан",
+      });
+    }
+  };
+};
