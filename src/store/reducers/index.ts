5e6291bf4bf04f53dbcd491b461fbf17057e7c28
RaayNoff <kraxtv471365@gmail.com>
2022-08-16 20:07:22 +0700

    Modified rootReducer to implement listReducer

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import { authorizationReducer } from "./authorizationReducer";
+import listsReducer from "./listsReducer";
 import { registrationReducer } from "./registrationReducer";
 
 export const rootReducer = combineReducers({
   authorization: authorizationReducer,
   registration: registrationReducer,
+  lists: listsReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
