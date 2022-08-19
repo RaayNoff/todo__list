113cbc7502d825d2094ed8db997d9910ba427d05
Clicker1337 <lolololo.lol@list.ru>
2022-08-20 02:26:20 +0700

    Added new component Task

---
+++ src/components/UI/task/index.tsx
@@ -0,0 +1,30 @@
+import { FC } from "react";
+import { ITask } from "../../../types/task";
+import s from "./task.module.scss";
+
+interface TaskProps {
+  task: ITask;
+}
+
+const Task: FC<TaskProps> = ({ task }) => {
+  return (
+    <section className={s.task}>
+      <div className={s.task__checkbox}></div>
+
+      <section className={s.task__data}>
+        <p className={s.task__name}>{task.taskName}</p>
+        <p className={s.task__description}> {task.description} </p>
+        <section className={s.task__footer}>
+          <section className={s.task__date}>
+            <div></div>
+            <p>{task.endTime} </p>
+          </section>
+          <p className={s.task__from}>Задачи с Дмитрием</p>
+        </section>
+      </section>
+      
+    </section>
+  );
+};
+
+export default Task;
