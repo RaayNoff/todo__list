fd1b18b2652e5f8870c16621dad3136bcc1b7f49
RaayNoff <kraxtv471365@gmail.com>
2022-08-21 19:31:49 +0700

    Added enums for future tips on registration

---
+++ src/types/validator.ts
@@ -0,0 +1,6 @@
+export enum EmailValidateAnswers {
+  CORRECT_EMAIL = "Введенная почта удовлетворяет требованиям",
+  INCORRECT_EMAIL = "Указанная почта не удовлетворяет требованиям",
+}
+
+export enum PasswordValidateAnswers {}
