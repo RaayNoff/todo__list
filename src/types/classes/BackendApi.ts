3066e03d48a1e2b6c904e1257421761939cbc76f
RaayNoff <kraxtv471365@gmail.com>
2022-08-26 00:44:02 +0700

    Changed constants

--- src/types/classes/BackendApi.ts
+++ src/types/classes/BackendApi.ts
@@ -1,9 +1,10 @@
 export default class BackendApi {
-  static LOCATION = "https://37.22.100.251:8443";
+  static LOCATION = "https://95.190.28.13:8843";
 
   static REGISTRATION = `${this.LOCATION}/api/auth/signup`;
   static LOGIN = `${this.LOCATION}/api/auth/signin`;
   static FETCH_LISTS = `${this.LOCATION}/api/lists/all`;
   static REFRESH = `${this.LOCATION}/api/auth/refresh`;
   static TASK = "/api/task";
+  static LIST = "/api/list";
 }
