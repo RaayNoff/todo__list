97249bf1cebd613ea1a5d03fbb70b75fa2115dcc
RaayNoff <kraxtv471365@gmail.com>
2022-08-26 00:39:45 +0700

    Concated listApi middleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { listApi } from "../services/listApi";
 import { rootReducer } from "./reducers";
 
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(listApi.middleware),
 });
 
 export type AppStore = typeof store;
