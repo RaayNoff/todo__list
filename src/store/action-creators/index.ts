113cbf2ecf27eb38b75c5fff3f96972fa9f61e00
RaayNoff <kraxtv471365@gmail.com>
2022-08-26 00:39:04 +0700

    Added popup action-creators

--- src/store/action-creators/index.ts
+++ src/store/action-creators/index.ts
@@ -1,7 +1,9 @@
 import * as AuthorizationActionCreators from "./authorization";
 import * as ListsActionCreators from "./list";
+import * as PopupsActionCreators from "./popups";
 
 export default {
   ...AuthorizationActionCreators,
   ...ListsActionCreators,
+  ...PopupsActionCreators,
 };
