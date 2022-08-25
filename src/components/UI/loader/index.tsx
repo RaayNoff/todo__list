71ef691768afce18f3b5a227c5aa1690fec7024a
RaayNoff <kraxtv471365@gmail.com>
2022-08-25 19:52:47 +0700

    Added new type of loader

--- src/components/UI/loader/index.tsx
+++ src/components/UI/loader/index.tsx
@@ -1,17 +1,49 @@
-import { FC } from "react";
+import { FC, useRef } from "react";
+import { useColor } from "../../../hooks/useColor";
+import { LoaderType } from "../../../types/enums/LoaderType";
 import s from "./loader.module.scss";
 
 interface ILoaderProps {
   isActive?: boolean;
+  color?: string;
+  loaderType?: LoaderType;
 }
 
-const Loader: FC<ILoaderProps> = ({ isActive }) => {
+const Loader: FC<ILoaderProps> = ({
+  isActive,
+  color = "#fff",
+  loaderType = LoaderType.DEFAULT,
+}) => {
+  const svgRef = useColor(color);
+
   if (isActive)
-    return (
-      <svg className={s.svg} viewBox="25 25 50 50">
-        <circle className={s.circle} cx="50" cy="50" r="20"></circle>
-      </svg>
-    );
+    switch (loaderType) {
+      case LoaderType.DEFAULT:
+        return (
+          <svg className={s.svg} viewBox="25 25 50 50">
+            <circle
+              ref={svgRef}
+              className={s.circle}
+              cx="50"
+              cy="50"
+              r="20"
+            ></circle>
+          </svg>
+        );
+
+      case LoaderType.LIST:
+        return (
+          <div className={s.list}>
+            <span className={s.list__span}></span>
+            <span className={s.list__span}></span>
+            <span className={s.list__span}></span>
+            <span className={s.list__span}></span>
+          </div>
+        );
+
+      default:
+        return <svg></svg>;
+    }
 
   return <></>;
 };
