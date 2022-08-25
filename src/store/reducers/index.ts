e45157aaff10d6ed0167f1e144655524349aeb3c
RaayNoff <kraxtv471365@gmail.com>
2022-08-26 00:41:44 +0700

    Added new reducers and changed literals of old reducers

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,10 +1,14 @@
 import { combineReducers } from "redux";
+import { listApi } from "../../services/listApi";
 import list from "./listSlice";
 import authorization from "./authorizationSlice";
+import popups from "./popupsSlice";
 
 export const rootReducer = combineReducers({
-  authorization,
-  list,
+  popups: popups.reducer,
+  authorization: authorization.reducer,
+  list: list.reducer,
+  [listApi.reducerPath]: listApi.reducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
