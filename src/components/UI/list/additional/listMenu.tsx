e9044b8df80870d52c71f8b650d4a528bb919ef1
RaayNoff <kraxtv471365@gmail.com>
2022-08-26 00:30:20 +0700

    Implemented logic of displaying sharelist modal

--- src/components/UI/list/additional/listMenu.tsx
+++ src/components/UI/list/additional/listMenu.tsx
@@ -1,16 +1,42 @@
-import React, { FC } from "react";
+import React, { Dispatch, FC } from "react";
+import { useActions } from "../../../../hooks/useActions";
+import { listApi } from "../../../../services/listApi";
 import s from "./listMenu.module.scss";
 
 interface IListMenuProps {
   isEnabled: boolean;
+  setIsEnabled: Dispatch<React.SetStateAction<boolean>>;
+  listId: number;
 }
 
-const ListMenu: FC<IListMenuProps> = ({ isEnabled }) => {
+const ListMenu: FC<IListMenuProps> = ({ isEnabled, listId, setIsEnabled }) => {
+  const [deleteList, {}] = listApi.useDeleteListMutation();
+  const { shareListToggleOn, fetchLists } = useActions();
+
+  const deleteListHandler = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    setIsEnabled((prev) => !prev);
+    deleteList(listId);
+    fetchLists();
+  };
+
+  const shareListHandler = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    shareListToggleOn(listId);
+    setIsEnabled((prev) => !prev);
+  };
+
   return (
-    <section className={isEnabled ? `${s.listMenu__active}` : `${s.listMenu}`}>
+    <section
+      className={isEnabled ? `${s.listMenu} ${s.active}` : `${s.listMenu}`}
+    >
       <p className={s.listMenu__item}>Редактировать список</p>
-      <p className={s.listMenu__item}>Поделиться списком</p>
-      <p className={s.listMenu__item}>Удалить список</p>
+      <p className={s.listMenu__item} onClick={(e) => shareListHandler(e)}>
+        Поделиться списком
+      </p>
+      <p className={s.listMenu__item} onClick={(e) => deleteListHandler(e)}>
+        Удалить список
+      </p>
     </section>
   );
 };
