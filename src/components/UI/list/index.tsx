05f95e80e41763052cf3e374a00de84ab1f2768f
RaayNoff <kraxtv471365@gmail.com>
2022-08-27 18:35:19 +0700

    Optimized the code using new hooks

--- src/components/UI/list/index.tsx
+++ src/components/UI/list/index.tsx
@@ -1,5 +1,6 @@
-import { FC, useEffect, useMemo, useRef, useState } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import { useListById } from "../../../hooks/useListById";
+import { useMenuPosition } from "../../../hooks/useMenuPosition";
 import ListMenu from "./additional/listMenu";
 import s from "./list.module.scss";
 
@@ -9,10 +10,7 @@ interface IListProps {
 
 const List: FC<IListProps> = ({ listId }) => {
   const dotRef = useRef<HTMLDivElement>(null);
-  const [menuPosition, setMenuPosition] = useState({
-    offsetLeft: 0,
-    offsetTop: 0,
-  });
+  const menuPosition = useMenuPosition(dotRef);
   const [isMenuOpened, SetIsMenuOpened] = useState<boolean>(false);
   const { color, listName } = useListById(listId);
 
@@ -20,10 +18,6 @@ const List: FC<IListProps> = ({ listId }) => {
     if (dotRef.current) {
       const dotNode = dotRef.current;
       dotNode.style.backgroundColor = color;
-      setMenuPosition({
-        offsetLeft: dotNode.offsetLeft + 150,
-        offsetTop: dotNode.offsetTop + 15,
-      });
     }
   }, [color, dotRef]);
 
