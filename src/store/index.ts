11a61d40ad1993cc56c7057692b6389ac0852eef
RaayNoff <kraxtv471365@gmail.com>
2022-08-28 00:40:51 +0700

    Added taskApi middleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,14 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { listApi } from "../services/listApi";
+import { taskApi } from "../services/taskApi";
 import { rootReducer } from "./reducers";
 
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(listApi.middleware),
+    getDefaultMiddleware()
+      .concat(listApi.middleware)
+      .concat(taskApi.middleware),
 });
 
 export type AppStore = typeof store;
