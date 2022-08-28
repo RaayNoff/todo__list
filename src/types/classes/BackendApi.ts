8fc45815dc288a2ecfc37481f3a340545dfa2fc5
RaayNoff <kraxtv471365@gmail.com>
2022-08-28 23:17:25 +0700

    Changed backend API

--- src/types/classes/BackendApi.ts
+++ src/types/classes/BackendApi.ts
@@ -1,11 +1,11 @@
 export default class BackendApi {
-  static LOCATION = "https://95.190.28.13:8843";
+  static LOCATION = "https://26.202.142.249:8843";
 
   static REGISTRATION = `${this.LOCATION}/api/auth/signup`;
   static LOGIN = `${this.LOCATION}/api/auth/signin`;
-  static FETCH_LISTS = `${this.LOCATION}/api/lists/all`;
+  static FETCH_LISTS = `${this.LOCATION}/api/lists`;
   static REFRESH = `${this.LOCATION}/api/auth/refresh`;
   static TEST = `${this.LOCATION}/api/auth/cookie`;
   static TASK = "/api/task";
-  static LIST = "/api/list";
+  static LIST = "/api/lists/add";
 }
