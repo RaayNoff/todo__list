4735c97cf1d9277c38186b2ab5dbb5697cf496af
RaayNoff <kraxtv471365@gmail.com>
2022-08-29 23:03:57 +0700

    Added error messages enum

---
+++ src/types/enums/ErrorMessages.ts
@@ -0,0 +1,6 @@
+export enum ErrorMessages {
+  REGISTRATION = "Не удалось зарегистрироваться",
+  LOGIN = "Не удалось войти",
+  REFRESH_NOT_VALID = "Вам необходимо войти заново",
+  REFRESH = "Произошла ошибка при попытке обновить данные",
+}
