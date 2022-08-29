aeee9ee823ef1c649302bc5ef14e411d492ca17a
RaayNoff <kraxtv471365@gmail.com>
2022-08-29 23:19:44 +0700

    Styles changed

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,6 +1,7 @@
-import { FC, useEffect, useState } from "react";
+import { FC, useState } from "react";
 import { useActions } from "../../../hooks/useActions";
 import { useTypedSelector } from "../../../hooks/useTypedSelector";
+import { contentApi } from "../../../services/contentApi";
 import { MaxWidthContainer } from "../../../types/enums/MaxWidthContainer";
 import Header from "../../common/header";
 import HomeContent from "../../common/homeContent";
@@ -12,16 +13,14 @@ import s from "./home.module.scss";
 
 const Home: FC = () => {
   const [isSidebarDisplayed, setIsSidebarDisplayed] = useState<boolean>(true);
-
+  const { data: listss } = contentApi.useFetchAllListsQuery(0);
+  const { data: tasks } = contentApi.useFetchAllTasksQuery(0);
   const { fetchLists } = useActions();
   const { error, lists, loading } = useTypedSelector((state) => state.list);
 
   const toggleSidebar = () => {
     setIsSidebarDisplayed(!isSidebarDisplayed);
   };
-  useEffect(() => {
-    fetchLists();
-  }, []);
 
   return (
     <main>
