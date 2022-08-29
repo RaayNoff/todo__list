0020fd6eecb48dc802fd43d7b630116a16646cd7
RaayNoff <kraxtv471365@gmail.com>
2022-08-29 23:14:14 +0700

    Finished developing action creators for homeContent

--- src/store/action-creators/homeContent.ts
+++ src/store/action-creators/homeContent.ts
@@ -4,3 +4,11 @@ import homeContentSlice from "../reducers/homeContentSlice";
 export const displayToday = () => (dispatch: AppDispatch) => {
   dispatch(homeContentSlice.actions.displayToday());
 };
+
+export const displayFuture = () => (dispatch: AppDispatch) => {
+  dispatch(homeContentSlice.actions.displayFuture());
+};
+
+export const displayList = (listId: number) => (dispatch: AppDispatch) => {
+  dispatch(homeContentSlice.actions.displayList(listId));
+};
