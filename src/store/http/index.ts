48ffb407b5fb8256eb75aa04e024401e2f30a559
RaayNoff <kraxtv471365@gmail.com>
2022-08-29 23:13:35 +0700

    Added interceptors and other baseQuery for api

--- src/store/http/index.ts
+++ src/store/http/index.ts
@@ -1,7 +1,15 @@
+import {
+  BaseQueryFn,
+  FetchArgs,
+  fetchBaseQuery,
+  FetchBaseQueryError,
+} from "@reduxjs/toolkit/dist/query";
 import axios from "axios";
 import { useActions } from "../../hooks/useActions";
 import BackendApi from "../../types/classes/BackendApi";
+import { ErrorMessages } from "../../types/enums/ErrorMessages";
 import { AuthResponse } from "../../types/interfaces/Authorization";
+import AuthorizationSlice from "../reducers/authorizationSlice";
 
 export const $api = axios.create({
   withCredentials: true,
@@ -31,8 +39,8 @@ $api.interceptors.response.use(
     ) {
       originalRequest.isRetry = true;
       try {
-        const { checkAuth } = useActions();
-        checkAuth();
+        const { refresh } = useActions();
+        refresh();
         return $api.request(originalRequest);
       } catch (e: any) {
         console.error(e.message);
@@ -41,3 +49,45 @@ $api.interceptors.response.use(
     throw Error("Произошла ошибка при попытке обновить данные");
   }
 );
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: BackendApi.LOCATION,
+  prepareHeaders: (headers) => {
+    headers.set("Authorization", `Bearer ${localStorage.getItem("token")}`);
+    return headers;
+  },
+  credentials: "include",
+});
+
+export const baseQueryWithReauth: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result.error && result.error.status === 401) {
+    api.dispatch(AuthorizationSlice.actions.refresh());
+    const refreshResult = await baseQuery(
+      {
+        url: BackendApi.REFRESH,
+        method: "POST",
+      },
+      api,
+      extraOptions
+    );
+
+    const data: AuthResponse | unknown = refreshResult.data;
+
+    if (data) {
+      api.dispatch(AuthorizationSlice.actions.refreshSuccess());
+
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(
+        AuthorizationSlice.actions.refreshError(ErrorMessages.REFRESH)
+      );
+    }
+  }
+  return result;
+};
