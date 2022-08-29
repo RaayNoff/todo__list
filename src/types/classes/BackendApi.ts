9b4e0971607696c5a1514ff28c6817ca1dfa859c
RaayNoff <kraxtv471365@gmail.com>
2022-08-29 23:08:47 +0700

    Extended BackendApi with new fields

--- src/types/classes/BackendApi.ts
+++ src/types/classes/BackendApi.ts
@@ -1,11 +1,19 @@
 export default class BackendApi {
   static LOCATION = "https://26.202.142.249:8843";
 
-  static REGISTRATION = `${this.LOCATION}/api/auth/signup`;
-  static LOGIN = `${this.LOCATION}/api/auth/signin`;
-  static FETCH_LISTS = `${this.LOCATION}/api/lists`;
-  static REFRESH = `${this.LOCATION}/api/auth/refresh`;
-  static TEST = `${this.LOCATION}/api/auth/cookie`;
-  static TASK = "/api/task";
-  static LIST = "/api/lists/add";
+  static REGISTRATION = "/api/auth/signup";
+  static LOGIN = "/api/auth/signin";
+  static REFRESH = "/api/auth/refresh";
+
+  static FETCH_LISTS = "/api/lists";
+  static FETCH_LISTS_ADD = `${this.FETCH_LISTS}/add`;
+  static FETCH_LISTS_EDIT = `${this.FETCH_LISTS}/edit`;
+  static FETCH_LISTS_DELETE = `${this.FETCH_LISTS}/del`;
+  static FETCH_LISTS_SHARE = `${this.FETCH_LISTS}/share`;
+
+  static FETCH_TASKS = "/api/tasks";
+  static FETCH_TASKS_ADD = `${this.FETCH_TASKS}/add`;
+  static FETCH_TASKS_EDIT = `${this.FETCH_TASKS}/edit`;
+  static FETCH_TASKS_COMMENT = `${this.FETCH_TASKS}/comment`;
+  static FETCH_TASKS_DELETE = `${this.FETCH_TASKS}/del`;
 }
