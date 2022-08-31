bd6085f0481d52465d900173b561964ecf2af3be
RaayNoff <kraxtv471365@gmail.com>
2022-08-31 16:24:55 +0700

    Changed path of signup and signin, added common styles for both

--- src/components/pages/nonauthorized/singup/index.tsx
+++ src/components/pages/nonauthorized/singup/index.tsx
@@ -1,10 +1,10 @@
 import React, { FC } from "react";
-import Header from "../../common/header";
-import checklist from "../../../icons/welcomeImg.jpg";
-import AuthorizationForm from "../../UI/authorizationForm";
-import { useActions } from "../../../hooks/useActions";
-import { MaxWidthContainer } from "../../../types/enums/MaxWidthContainer";
-import s from "./signUp.module.scss";
+import Header from "../../../common/header";
+import checklist from "../../../../icons/welcomeImg.jpg";
+import AuthorizationForm from "../../../UI/authorizationForm";
+import { useActions } from "../../../../hooks/useActions";
+import { MaxWidthContainer } from "../../../../types/enums/MaxWidthContainer";
+import s from "../noauthorized.module.scss";
 
 const SingUp: FC = () => {
   const { registration } = useActions();
@@ -12,14 +12,18 @@ const SingUp: FC = () => {
   return (
     <main>
       <Header></Header>
-      <section className={s.SingUp}>
+      <section className={s.sign}>
         <div className={MaxWidthContainer.NON_AUTHORIZED}>
-          <section className={s.SingUp__content}>
+          <section className={s.sign__content}>
             <section className={s.welcome}>
               <header className={s.welcome__message}>
                 Добро пожаловать на сайт <br /> todoLIST
               </header>
-              <img src={checklist} alt="checklist"></img>
+              <img
+                className={s.welcome__img}
+                src={checklist}
+                alt="checklist"
+              ></img>
             </section>
             <AuthorizationForm fetchCallback={registration} />
           </section>
