4f9672a0d81c09cfdc443eb83ad03a7368ed39b0
RaayNoff <kraxtv471365@gmail.com>
2022-09-04 00:10:16 +0700

    initialState use safe values now. And setLastDeletedTask reducer added.

--- src/store/reducers/popupsSlice.ts
+++ src/store/reducers/popupsSlice.ts
@@ -4,16 +4,17 @@ import { IPopupsState } from "../../types/states/IPopupsState";
 const initialState: IPopupsState = {
   createList: false,
   taskInfo: {
-    status: true,
-    currentTaskId: 1,
+    status: false,
+    currentTaskId: -1,
+    lastDeletedTaskId: -2,
   },
   shareList: {
     status: false,
-    currentListId: 1,
+    currentListId: -1,
   },
   editList: {
     status: false,
-    currentlistId: 1,
+    currentlistId: -1,
   },
 };
 
@@ -28,10 +29,11 @@ const PopupsSlice = createSlice({
       state.createList = false;
     },
     taskInfoToggleOn(state, action: PayloadAction<number>) {
-      state.taskInfo.currentTaskId = action.payload;
       state.taskInfo.status = true;
+      state.taskInfo.currentTaskId = action.payload;
     },
     taskInfoToggleOff(state) {
+      state.taskInfo.currentTaskId = -1;
       state.taskInfo.status = false;
     },
     shareListToggleOn(state, action: PayloadAction<number>) {
@@ -40,7 +42,7 @@ const PopupsSlice = createSlice({
     },
     shareListToggleOff(state) {
       state.shareList.status = false;
-      state.shareList.currentListId = 1;
+      state.shareList.currentListId = -1;
     },
     editListToggleOn(state, action: PayloadAction<number>) {
       state.editList.status = true;
@@ -48,6 +50,10 @@ const PopupsSlice = createSlice({
     },
     editListToggleOff(state) {
       state.editList.status = false;
+      state.editList.currentlistId = -1;
+    },
+    setLastDeletedTask(state, action: PayloadAction<number>) {
+      state.taskInfo.lastDeletedTaskId = action.payload;
     },
   },
 });
