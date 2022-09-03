9f6d28d5be4968de408b36855e92a89306f879f2
RaayNoff <kraxtv471365@gmail.com>
2022-09-04 00:06:55 +0700

    New sidebar action-creators.

--- src/store/action-creators/index.ts
+++ src/store/action-creators/index.ts
@@ -1,9 +1,11 @@
 import * as AuthorizationActionCreators from "./authorization";
 import * as PopupsActionCreators from "./popups";
 import * as HomeContentActionCreators from "./homeContent";
+import * as SidebarActionCreators from "./sidebar";
 
 export default {
   ...AuthorizationActionCreators,
   ...PopupsActionCreators,
   ...HomeContentActionCreators,
+  ...SidebarActionCreators,
 };
