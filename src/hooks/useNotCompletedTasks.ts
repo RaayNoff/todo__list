9c9b7ecc9fef343202ab529d97d6232f126e4bbf
RaayNoff <kraxtv471365@gmail.com>
2022-09-04 00:01:16 +0700

    Added if expression.

--- src/hooks/useNotCompletedTasks.ts
+++ src/hooks/useNotCompletedTasks.ts
@@ -1,5 +1,7 @@
 import { ITask } from "../types/models/ITask";
 
-export const useNotCompletedTasks = (tasks: ITask[]) => {
+export const useNotCompletedTasks = (tasks: ITask[] | null) => {
+  if (!tasks) return [] as ITask[];
+
   return tasks.filter((task) => !task.status);
 };
