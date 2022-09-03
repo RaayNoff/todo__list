c351ac77920965adaa18d558c0aebca675dbfe74
RaayNoff <kraxtv471365@gmail.com>
2022-09-03 23:48:10 +0700

    Added default navigate after registration.

--- src/components/UI/authorizationForm/index.tsx
+++ src/components/UI/authorizationForm/index.tsx
@@ -1,10 +1,11 @@
 import { FC, SyntheticEvent, useState } from "react";
-import { useActions } from "../../../hooks/useActions";
+import { useNavigate } from "react-router-dom";
 import usePath from "../../../hooks/usePath";
 import { useTypedSelector } from "../../../hooks/useTypedSelector";
 import useValidData from "../../../hooks/useValidData";
 import { AuthorizationType } from "../../../store/action-creators/authorization";
 import { FormInputType } from "../../../types/enums/FormInputType";
+import { RoutePath } from "../../../types/enums/RoutePath";
 import FormFooter from "./additional/formFooter";
 import FormHeader from "./additional/formHeader";
 import FormInput from "./additional/formInput";
@@ -18,40 +19,37 @@ interface IAuthorizationProps {
 const AuthorizationForm: FC<IAuthorizationProps> = ({ fetchCallback }) => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
-  const { refresh } = useActions();
   const { error } = useTypedSelector((state) => state.authorization);
   const isDataValid = useValidData(email, password);
   const isRegistration = usePath();
-
+  const navigate = useNavigate();
   const onClickHandler = (e: SyntheticEvent): void => {
     e.preventDefault();
 
     fetchCallback(email, password);
-    if (isRegistration) refresh();
+
+    if (isRegistration) {
+      navigate(RoutePath.SIGNIN);
+    }
   };
 
   return (
-    <section className={s.authorization}>
-      <form className={s.form}>
-        <FormHeader />
-        <FormInput
-          inputType={FormInputType.EMAIL}
-          value={email}
-          onChangeValue={setEmail}
-        />
-        <FormInput
-          inputType={FormInputType.PASSWORD}
-          value={password}
-          onChangeValue={setPassword}
-        />
-        <p className={s.form__error}>{error}</p>
-        <ResponseSection
-          callback={onClickHandler}
-          canBeClicked={!isDataValid}
-        />
-        <FormFooter />
-      </form>
-    </section>
+    <form className={s.form}>
+      <FormHeader />
+      <FormInput
+        inputType={FormInputType.EMAIL}
+        value={email}
+        onChangeValue={setEmail}
+      />
+      <FormInput
+        inputType={FormInputType.PASSWORD}
+        value={password}
+        onChangeValue={setPassword}
+      />
+      <p className={s.form__error}>{error}</p>
+      <ResponseSection callback={onClickHandler} canBeClicked={!isDataValid} />
+      <FormFooter />
+    </form>
   );
 };
 
