580f50c4f1e15188e7b7cbe0e1e95351e3447770
RaayNoff <kraxtv471365@gmail.com>
2022-09-04 00:00:47 +0700

    New if expression.

--- src/hooks/useListByTaskId.ts
+++ src/hooks/useListByTaskId.ts
@@ -1,12 +1,11 @@
 import { contentApi } from "../services/contentApi";
 import { IList } from "../types/models/IList";
 import { notList } from "../types/noData";
-import { useTypedSelector } from "./useTypedSelector";
 
 export const useListByTaskId = (taskId: number): IList => {
   const { data: lists } = contentApi.useFetchAllListsQuery(0);
 
-  if (!lists) return notList;
+  if (!lists || lists.length < 1 || taskId === -1) return notList;
 
   const _temp: IList[] = [];
 
