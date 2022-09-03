aaf4023690f4d7548613a3abfcac226fbe6f83fb
RaayNoff <kraxtv471365@gmail.com>
2022-09-04 00:01:41 +0700

    Added if expressions to avoid incorrect data return.

--- src/hooks/useTaskById.ts
+++ src/hooks/useTaskById.ts
@@ -1,11 +1,21 @@
 import { contentApi } from "../services/contentApi";
 import { ITask } from "../types/models/ITask";
 import { notTask } from "../types/noData";
+import { useTypedSelector } from "./useTypedSelector";
 
 export const useTaskById = (taskId: number): ITask => {
   const { data: tasks } = contentApi.useFetchAllTasksQuery(0);
+  const {
+    taskInfo: { lastDeletedTaskId },
+  } = useTypedSelector((state) => state.popups);
 
-  if (!tasks) return notTask;
+  if (
+    !tasks ||
+    tasks.length < 1 ||
+    taskId === -1 ||
+    taskId === lastDeletedTaskId
+  )
+    return notTask;
 
   const _temp: ITask[] = [];
 
