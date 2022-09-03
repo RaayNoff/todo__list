b2a97d901f5abe237e99ed88b30e2c21f51d79c0
RaayNoff <kraxtv471365@gmail.com>
2022-09-04 00:10:59 +0700

    Objects updated.

--- src/types/noData.ts
+++ src/types/noData.ts
@@ -7,20 +7,31 @@ export const noFiltredObject = {
 };
 
 export const notList: IList = {
-  color: "red",
-  id: 1,
-  listName: "No name",
-  accessedUsers: [],
-  tasks: [],
+  color: "#fff",
+  id: -1,
+  listName: " ",
+  accessedUsers: [
+    {
+      email: " ",
+    },
+  ],
+  tasks: [
+    {
+      id: -1,
+      taskName: "",
+      status: false,
+      description: "",
+      comments: [],
+      endTime: 1,
+    },
+  ],
 };
 
 export const notTask: ITask = {
   id: 1,
-  taskName:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  taskName: " ",
   comments: [],
-  description:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  description: " ",
   endTime: 1,
   status: false,
 };
