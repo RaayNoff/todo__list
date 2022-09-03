302162474119a134ede10a7316d819105853251f
RaayNoff <kraxtv471365@gmail.com>
2022-09-04 00:02:15 +0700

    Added if expressions.

--- src/hooks/useTasksByListId.ts
+++ src/hooks/useTasksByListId.ts
@@ -1,10 +1,10 @@
 import { contentApi } from "../services/contentApi";
-import { notTaskArr } from "../types/noData";
+import { ITask } from "../types/models/ITask";
 
 export const useTasksByListId = (listId: number) => {
   const { data: lists } = contentApi.useFetchAllListsQuery(0);
 
-  if (!lists) return notTaskArr;
+  if (!lists || lists.length < 1 || listId === -1) return [] as ITask[];
 
   const [requiredList] = lists.filter((list) => list.id === listId);
 
