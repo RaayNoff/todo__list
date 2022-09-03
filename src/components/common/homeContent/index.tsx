218e0793c3ca5c36a8b5b595cf90b51ec54a8535
RaayNoff <kraxtv471365@gmail.com>
2022-09-03 23:26:19 +0700

    Added selector switch when sidebar enabled or not.

--- src/components/common/homeContent/index.tsx
+++ src/components/common/homeContent/index.tsx
@@ -1,11 +1,21 @@
+import { useTypedSelector } from "../../../hooks/useTypedSelector";
 import { MaxWidthContainer } from "../../../types/enums/MaxWidthContainer";
 import Switcher from "./additional/switcher";
 import s from "./homeContent.module.scss";
 
 const HomeContent = () => {
+  const { status: isSidebarEnabled } = useTypedSelector(
+    (state) => state.sidebar
+  );
   return (
     <main className={s.homeContent}>
-      <div className={MaxWidthContainer.HOME_CONTENT}>
+      <div
+        className={
+          isSidebarEnabled
+            ? `${MaxWidthContainer.HOME_CONTENT} ${MaxWidthContainer.HOME_CONTENT}__se`
+            : `${MaxWidthContainer.HOME_CONTENT}`
+        }
+      >
         <section className={s.homeContent__content}>
           <Switcher />
         </section>
