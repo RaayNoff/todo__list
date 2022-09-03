5eada88863dd1fe01d5776875e764f63b3494bdb
RaayNoff <kraxtv471365@gmail.com>
2022-09-03 23:48:20 +0700

    Removed unused imports

--- src/components/UI/authorizationForm/additional/formFooter.tsx
+++ src/components/UI/authorizationForm/additional/formFooter.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import { FC } from "react";
 import { Link } from "react-router-dom";
 import usePath from "../../../../hooks/usePath";
 import s from "./formFooter.module.scss";
