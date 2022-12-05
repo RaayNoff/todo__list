4121c2b1201e3e1bfcf4cce58f4c6bc83de454ff
RaayNoff <kraxtv471365@gmail.com>
2022-12-05 16:11:04 +0700

    Ready for deploy changes

--- src/components/modals/shareList/index.tsx
+++ src/components/modals/shareList/index.tsx
@@ -1,5 +1,4 @@
-import React, { FC } from "react";
-import { useState } from "react";
+import React, { FC, useState } from "react";
 
 import { ButtonTypes } from "../../../types/enums/ButtonTypes";
 import { InputSizeTypes } from "../../../types/enums/InputSizeTypes";
@@ -18,68 +17,68 @@ import NoInfo from "./additional/noInfo";
 import AccessedUsers from "./additional/accessedUsers";
 
 const ShareList: FC = () => {
-  const [userEmail, setUserEmail] = useState<string>("");
-  const { shareList } = useTypedSelector((state) => state.popups);
-  const { shareListToggleOff } = useActions();
-  const [shareWithUser] = contentApi.useFetchListsShareMutation();
-  const { accessedUsers } = useListById(shareList.currentListId);
+	const [userEmail, setUserEmail] = useState<string>("");
+	const { shareList } = useTypedSelector((state) => state.popups);
+	const { shareListToggleOff } = useActions();
+	const [shareWithUser] = contentApi.useFetchListsShareMutation();
+	const { accessedUsers } = useListById(shareList.currentListId);
 
-  const checkAccessedUsers = (users: IUser[] = accessedUsers): boolean => {
-    if (users.length < 1) return false;
+	const checkAccessedUsers = (users: IUser[] = accessedUsers): boolean => {
+		if (users.length < 1) return false;
 
-    return true;
-  };
+		return true;
+	};
 
-  const onInviteHandler = (e: React.MouseEvent) => {
-    shareWithUser({ email: userEmail, listId: shareList.currentListId });
-    setUserEmail("");
-  };
+	const onInviteHandler = (e: React.MouseEvent) => {
+		shareWithUser({ email: userEmail, listId: shareList.currentListId });
+		setUserEmail("");
+	};
 
-  return (
-    <div
-      className={shareList.status ? "popup enabled" : "popup"}
-      onClick={() => shareListToggleOff()}
-    >
-      <form className={s.shareList} onClick={(e) => e.stopPropagation()}>
-        <HeaderInsert>Добавить пользователя</HeaderInsert>
-        <main
-          className={
-            checkAccessedUsers()
-              ? `${s.shareList__content} ${s.mb146}`
-              : `${s.shareList__content} ${s.mb113}`
-          }
-        >
-          <section
-            className={
-              checkAccessedUsers()
-                ? `${s.shareList__clientResponse} ${s.mb36}`
-                : `${s.shareList__clientResponse} ${s.mb89}`
-            }
-          >
-            <Input
-              size={InputSizeTypes.MEDIUM}
-              inputValue={userEmail}
-              onChangeCallback={setUserEmail}
-              title="Введите Email пользователя"
-              placeholder="example@mail.ru"
-            ></Input>
-            <Button
-              btnType={ButtonTypes.ACTION}
-              text="Пригласить"
-              onClickCallback={onInviteHandler}
-            />
-          </section>
+	return (
+		<div
+			className={shareList.status ? "popup enabled" : "popup"}
+			onClick={() => shareListToggleOff()}
+		>
+			<form className={s.shareList} onClick={(e) => e.stopPropagation()}>
+				<HeaderInsert>Добавить пользователя</HeaderInsert>
+				<main
+					className={
+						checkAccessedUsers()
+							? `${s.shareList__content} ${s.mb146}`
+							: `${s.shareList__content} ${s.mb113}`
+					}
+				>
+					<section
+						className={
+							checkAccessedUsers()
+								? `${s.shareList__clientResponse} ${s.mb36}`
+								: `${s.shareList__clientResponse} ${s.mb89}`
+						}
+					>
+						<Input
+							size={InputSizeTypes.MEDIUM}
+							inputValue={userEmail}
+							onChangeCallback={setUserEmail}
+							title="Введите Email пользователя"
+							placeholder="example@mail.ru"
+						></Input>
+						<Button
+							btnType={ButtonTypes.ACTION}
+							text="Пригласить"
+							onClickCallback={onInviteHandler}
+						/>
+					</section>
 
-          {accessedUsers.length >= 1 ? (
-            <AccessedUsers accessedUsers={accessedUsers} />
-          ) : (
-            <NoInfo />
-          )}
-        </main>
-        <FooterInsert />
-      </form>
-    </div>
-  );
+					{accessedUsers.length >= 1 ? (
+						<AccessedUsers accessedUsers={accessedUsers} />
+					) : (
+						<NoInfo />
+					)}
+				</main>
+				<FooterInsert />
+			</form>
+		</div>
+	);
 };
 
 export default ShareList;
--- src/hooks/useTypedSelector.ts
+++ src/hooks/useTypedSelector.ts
@@ -1,5 +1,4 @@
-import { useSelector } from "react-redux";
-import { TypedUseSelectorHook } from "react-redux";
+import { useSelector, TypedUseSelectorHook } from "react-redux";
 
 import { RootState } from "../store/reducers";
 
